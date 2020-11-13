a01b135836e36cf0bffbcd105128d251ae0a1d5a
Tracy Marshall <tracythedev@gmail.com>
2020-11-12 23:12:26 -0500

    search box is now a component, added styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { Component } from 'react';
 
+import { SearchBox } from './components/search-box/search-box.component';
+
 import { CardList } from './components/card-list/card-list.component';
 
 import './App.css';
@@ -20,13 +22,23 @@ class App extends Component {
       .then(users => this.setState({ monsters: users}))
   }
 
+  handleChange = e => {
+    this.setState({ searchField: e.target.value })
+  }
+
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter( monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    )
     return (
       <div className="App">
-        <input type='search' placeholder='search monsters' onChange={e => console.log(e)}/>
-        <CardList monsters={this.state.monsters}> 
-          
-        </CardList>
+        <h1> Monsters Rolodex </h1>
+        <SearchBox
+          placeholder='search monsters'
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters} />
         
       </div>
     );
