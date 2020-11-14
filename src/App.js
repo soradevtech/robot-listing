39b25c14794fbed8f3cf1d961836b7e608de560a
Tracy Marshall <tracythedev@gmail.com>
2020-11-14 01:53:36 -0500

    updated package.json

--- src/App.js
+++ src/App.js
@@ -22,8 +22,8 @@ class App extends Component {
       .then(users => this.setState({ monsters: users}))
   }
 
-  handleChange = e => {
-    this.setState({ searchField: e.target.value })
+  onSearchChange = event => {
+    this.setState({ searchField: event.target.value, title: event.target.value })
   }
 
   render() {
@@ -36,7 +36,7 @@ class App extends Component {
         <h1> Monsters Rolodex </h1>
         <SearchBox
           placeholder='search monsters'
-          handleChange={this.handleChange}
+          handleChange={this.onSearchChange}
         />
         <CardList monsters={filteredMonsters} />
         
